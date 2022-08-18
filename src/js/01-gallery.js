7e0c01057985cdce53e9965aecf65bd86bf6896f
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2022-08-18 23:22:41 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const listGallery = document.querySelector('.gallery');
+
+const newArray = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li><a class="gallery__item" href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    alt="${description}">
+    </a></li>`;
+  })
+  .join('');
+
+listGallery.insertAdjacentHTML('afterbegin', newArray);
+
+const galerySimple = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+console.log(galerySimple);
