73a09c58f0182aea274fc305174ddf89e418af06
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2022-08-20 19:00:20 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe, {
+  id: 19231868,
+  width: 640,
+});
+
+newCurrentTime();
+
+player.on('timeupdate', throttle(playerTimeUpdate, 1000));
+
+function playerTimeUpdate() {
+  player
+    .getCurrentTime()
+    .then(seconds => localStorage.setItem('videoplayer-current-time', seconds));
+}
+
+function newCurrentTime() {
+  const newTimeUpdate = localStorage.getItem('videoplayer-current-time');
+  if (newTimeUpdate) {
+    player.setCurrentTime(newTimeUpdate);
+  }
+}
